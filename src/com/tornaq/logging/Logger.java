035d334e114139a02ab983769fbe91e27b737fee
Waldman <Waldman>
2015-11-16 06:37:45 +0200

    Autowire privet props W/ annotation

--- src/com/tornaq/logging/Logger.java
+++ src/com/tornaq/logging/Logger.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Created by dovw on 11/15/15.
  */
 public class Logger {
-
+    @Autowired
     private FileWriter fileWriter;
-
+    @Autowired
     private ConsoleWriter consoleWriter;
 
     public Logger() {}
-    @Autowired
+
     public Logger(FileWriter fileWriter, ConsoleWriter consoleWriter) {
         this.fileWriter = fileWriter;
         this.consoleWriter = consoleWriter;
@@ -22,17 +22,17 @@ public class Logger {
         return fileWriter;
     }
 
-    public void setFileWriter(FileWriter fileWriter) {
-        this.fileWriter = fileWriter;
-    }
+//    public void setFileWriter(FileWriter fileWriter) {
+//        this.fileWriter = fileWriter;
+//    }
 
     public ConsoleWriter getConsoleWriter() {
         return consoleWriter;
     }
 
-    public void setConsoleWriter(ConsoleWriter consoleWriter) {
-        this.consoleWriter = consoleWriter;
-    }
+//    public void setConsoleWriter(ConsoleWriter consoleWriter) {
+//        this.consoleWriter = consoleWriter;
+//    }
 
     public void log(String message) {
         fileWriter.log(message);
